e13a2e6ab5e18d11b1d8d1abd2704308f13fbddb
satooooh <st.tomonori@gmail.com>
2017-06-25 20:16:59 +0900

    簡易注文機能追加

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+class ProductsController < ApplicationController
+  def index
+    @product = Product.new
+  end
+
+  def create
+    product = Product.create(name: product_params[:name], user_id: current_user.id)
+    # PostMailer.post_email(current_user, @product).deliver_now
+  end
+
+private
+  def product_params
+    params.permit(:name)
+  end
+
+  def move_to_index
+    redirect_to "/" unless user_signed_in?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170625054509) do
+ActiveRecord::Schema.define(version: 20170625110858) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -71,6 +71,13 @@ ActiveRecord::Schema.define(version: 20170625054509) do
   create_table "hoges", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.integer  "age",        limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "user_id",    limit: 4
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
